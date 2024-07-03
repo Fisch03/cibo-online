c10baddd3ed7ee897b09f8e4faa5dbb88d5153a1
lars <lars@meuser.it>
2024-07-03 19:01:59 +0200

    working framebuffer

--- cibo_online/src/lib.rs
+++ cibo_online/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![no_std]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use monos_gfx::Dimension;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub const GAME_DIMENSIONS: Dimension = Dimension::new(640, 480);
--- web_client/src/lib.rs
+++ web_client/src/lib.rs
@@ -1,18 +1,61 @@
+use std::ptr::addr_of_mut;
+
 use wasm_bindgen::prelude::*;
+use monos_gfx::{Framebuffer, FramebufferFormat, Color, Position, Dimension, fonts, Rect};
 
-#[wasm_bindgen(start)]
-fn main() -> Result<(), JsValue> {
-    // Use `web_sys`'s global `window` function to get a handle on the global
-    // window object.
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
 
-    // Manufacture the element we're gonna append
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust!");
+const FB_SIZE: usize = cibo_online::GAME_DIMENSIONS.width as usize * cibo_online::GAME_DIMENSIONS.height as usize * 4;
 
-    body.append_child(&val)?;
+static mut RAW_FB: [u8; FB_SIZE] = [0u8; FB_SIZE];
 
-    Ok(())
+/// get a mutable reference to the raw framebuffer
+///
+/// safety: this is safe as long as it is done only once. since we're in a wasm environment, there can be no threading issues.
+// yeah, this is kind of horrible but the way the monos_gfx::Framebuffer is designed makes the alternatives really ugly
+unsafe fn raw_fb() -> &'static mut [u8; FB_SIZE] {
+    unsafe { &mut *addr_of_mut!(RAW_FB) }
+}
+
+#[wasm_bindgen]
+struct Game {
+    width: u32,
+    height: u32,
+    framebuffer: Framebuffer<'static>,
+}
+
+#[wasm_bindgen]
+impl Game {
+    pub fn new() -> Self {
+        let format = FramebufferFormat {
+            bytes_per_pixel: 4,
+            stride: cibo_online::GAME_DIMENSIONS.width as u64,
+            r_position: 0,
+            g_position: 1,
+            b_position: 2,
+            a_position: Some(3)
+        };
+
+        // safety: this is the only time we're accessing the raw framebuffer
+        let mut framebuffer = Framebuffer::new(unsafe {raw_fb()}, cibo_online::GAME_DIMENSIONS, format);
+        framebuffer.clear_alpha();
+        framebuffer.draw_str::<fonts::Cozette>(&Color::new(255, 255, 255), "Hello, world!",  &Position::new(0, 0));
+
+        Self {
+            width: cibo_online::GAME_DIMENSIONS.width,
+            height: cibo_online::GAME_DIMENSIONS.height,
+            framebuffer,
+        }
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn get_framebuffer(&self) -> *const u8 {
+        self.framebuffer.buffer().as_ptr()
+    }
 }
